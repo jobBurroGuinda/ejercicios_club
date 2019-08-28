ba41a81b6fb15dff82d31b3caa5fc6e8c140c5ba
jobBurroGuinda <54601244+jobBurroGuinda@users.noreply.github.com>
2019-08-28 15:49:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 
 using namespace std;
-int n, x, y, z;
+int n=0, x=0, y=0, z=0;
+int cx=0;
+int cy=0;
+int cz=0;
 int main()
 {
-        count<< n
+        cin>>n;
     while(n--){
-        count<< x
-        count<< y
-        count<< z
-        x += x;
-        y += y;
-        z += z;
+        cin>>x;
+        cin>>y;
+        cin>>z;
+        cx += x;
+        cy += y;
+        cz += z;
     }
-    if(x!=0 && y!=0 && z!=0){
-        printf("NO");
+    if(cx==0 && cy==0 && cz==0){
+        cout<<"YES";
     } else {
-        printf("YES");
+        cout<<"NO";
     }
 
     return 0;
