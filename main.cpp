99dac32fb36c9c93f6f380d14a34896db211466f
jobBurroGuinda <54601244+jobBurroGuinda@users.noreply.github.com>
2019-08-28 15:18:44 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int n, x, y, z;
+int main()
+{
+        count<< n
+    while(n--){
+        count<< x
+        count<< y
+        count<< z
+        x += x;
+        y += y;
+        z += z;
+    }
+    if(x!=0 && y!=0 && z!=0){
+        printf("NO");
+    } else {
+        printf("YES");
+    }
+
+    return 0;
+}
